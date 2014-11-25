ac99a306c66e0a3a7efabb4e5b9824d23ced8aeb
fazhaa <nospamtimtime@gmail.com>
2014-11-24 23:04:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
-#include "racer.h"
+#include<fstream>
+#include<iomanip>
+#include "Racer.h"
 
 using namespace std;
 
+bool readHash(HashedDictionary<string, Racer>* dict, string &inputFIleName, int &numElem);
 int main()
 {
   //size of hash table
-  const int TABLE_SIZE = 10;
+  //const int TABLE_SIZE = 10; this will be in the hash table adt file
   
-  racer* driver1 = new racer;
-  racer->buildHashTable( TABLE_SIZE );
+  //racer* driver1 = new racer;
+  //racer->buildHashTable( TABLE_SIZE );
 
   return 0;
+}
+
+
+bool readHash(HashedDictionary<string, Racer>* dict, string &inputFIleName, int &numElem)
+{
+  bool created = false;
+  bool inserted = false;
+  int i = 0;
+  int stringIterator = 0;
+  int newDate;
+  string 
+  
 }
