930f7cd5ff244380ea2ee0a28fd223706cd73d2e
fazhaa <nospamtimtime@gmail.com>
2014-11-25 18:06:27 -0800

    Update BinaryNode.h

--- BinaryNode.h
+++ BinaryNode.h
@@ -10,23 +10,23 @@ template<class ItemType>
 class BinaryNode
 {   
 private:
-	ItemType              item;         // Data portion
+	ItemType*              item;         // Data portion
 	BinaryNode<ItemType>* leftPtr;		// Pointer to left child
 	BinaryNode<ItemType>* rightPtr;		// Pointer to right child
 
 public:
 	// constructors
 	BinaryNode() { leftPtr = nullptr; rightPtr = nullptr; }
-	BinaryNode(const ItemType & anItem)	{item = anItem; leftPtr = nullptr; rightPtr = nullptr;}
+	BinaryNode(const ItemType & anItem)	{item = &anItem; leftPtr = nullptr; rightPtr = nullptr;}
 	BinaryNode(const ItemType & anItem, 
 		       BinaryNode<ItemType>* left, 
-			   BinaryNode<ItemType>* right) {item = anItem; leftPtr = left; rightPtr = right;}
+			   BinaryNode<ItemType>* right) {item = &anItem; leftPtr = left; rightPtr = right;}
 	// accessors
-	void setItem(const ItemType & anItem) {item = anItem;}
+	void setItem(const ItemType & anItem) {item = &anItem;}
 	void setLeftPtr(BinaryNode<ItemType>* left) {leftPtr = left;}
     void setRightPtr(BinaryNode<ItemType>* right) {rightPtr = right;}
 	// mutators
-	ItemType getItem() const	 {return item;}
+	ItemType getItem() const	 {return *item;}
 	BinaryNode<ItemType>* getLeftPtr() const  {return leftPtr;}
 	BinaryNode<ItemType>* getRightPtr() const {return rightPtr;}
 
