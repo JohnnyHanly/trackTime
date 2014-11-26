451053a8383c16be2c6a08bfdac311f89710fc4a
Tim <cracknmyass@yahoo.com>
2014-11-25 21:25:14 -0800

    hmmm

--- racer.h
+++ racer.h
@@ -1,80 +1,70 @@
-//racer.h
-#include <time.h>
-#include <stdio.h> 
-#include  <string>
+// Here is the header file we can use with
+// ADT BST and ADT Hashed Dictionary.
+// Now we just need to work on a main :)
 
+// Let us assume that in the input file,
+// the data will be in this order
+// name, idnumber, circuitname, date, finishtime (one line each)
 
-/*
-toDo
+#ifndef RACER
+#define RACER
 
-verify date input data
-get rid of racer.cpp, its redundant and isn't compatible with templates
-can you delete the file? The function defs will go in the header file. -steven
-*/ 
+#include <string>
 
 using namespace std;
-struct raceProfile
-{
-	string name;
-	string driverID; 
-	string circuitName;
-	struct tm date;
-};
-// this is a rough draft for object data for the racer
 
-class racer
+class Racer
 {
 private:
-	//singly linked list that serves as hash table.
-	//each node has a pointer to a second linked list
-	//with overflow pointing at the head. 
-	struct node
-	{
-		node* next;
-		node* overflow;
-	};
-	int hashCount, objCount, empty;
-	node* head;
+  string* name;
+  string* idNum;
+  string* circuitName;
+  string* date;
+  string* finishTime;
+  
+  
 public:
-	racer();
-	~racer();
-	void buildHashTable( int MAX_SIZE );
+  Racer() { name = nullptr; idNum = nullptr; circuitName = nullptr;
+    date = nullptr; finishTime = nullptr; }
+  Racer(string newName) { name = &newName; idNum = nullptr; circuitName = nullptr;
+    date = nullptr; finishTime = nullptr; }
+  Racer(string newName, string newId) { name = &newName; idNum = &newId; 
+    circuitName = nullptr; date = nullptr; finishTime = nullptr; }
+  
+  double getFinishTime() const{ return *finishTime; }
+  string getName() const{ return *name; }
+  string getIdNum() const { return *idNum; }
+  string getCircuitName() const { return *circuitName; }
+  string getDate() const { return *date;}
+  
+  void setFinishTime(string newTime) { finishTime = &newTime; }
+  void setName(string nm) { name = &nm; }
+  void setIdNum(string newId) { idNum = &newId; }
+  void setCircuitName(string newCircuit) { circuitName = &newCircuit; }
+  void setDate(string newDate) { date = &newDate; }
+  
+  bool operator > (const Racer &right)
+  {
+    bool status = false;
+    if(*name > right.getName())
+      status = true;
+    return status;
+  }
+  bool operator < (const Racer &right)
+  {
+    bool status = false;
+    if(*name < right.getName())
+      status = true;
+    return status;
+  }
+  const Racer operator=(const Racer &right)
+  {
+    name = &right.getName();
+    idNum = &right.getIdNum();
+    circuitName = &right.getCircuitName();
+    date = &right.getDate();
+    finishTime = &right.getFinishTime();
+    return *this;
+  }
 };
-
-void racer::buildHashTable( int MAX_SIZE )
-{
-	node* newNode;
-	node* cur = head;
-	empty = MAX_SIZE;
-	for(int i = 0;i < MAX_TABLE; i++)
-	{
-		newNode = new node;
-		cur->next = newNode;
-		cur = newNode;
-		cur->taken = false;
-		cur->next = NULL;
-		
-		//sentinel for overflow in each node
-		cur->overflow = new node;
-		cur->overflow->next = NULL;
-		++count;
-	}
-}
-//comment
-
-
-//c_tor
-racer::racer()
-{
-	//first entry is a sentinel node
-	head = new node;
-	head->next = NULL;
-	head->prev = NULL;
-	head->overflow = NULL;
-	objCount = 0;
-}
-
-racer::~racer()
-{
-	//d_tor
-}
+#endif RACER
