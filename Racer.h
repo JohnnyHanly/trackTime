155050c782dc12cbbd79f26b26798d1bbcbc7d9b
Tim <cracknmyass@yahoo.com>
2014-11-25 21:27:37 -0800

    updated Racer.h

--- Racer.h
+++ Racer.h
@@ -20,7 +20,7 @@ private:
   string* idNum;
   string* circuitName;
   string* date;
-  double* finishTime;
+  string* finishTime;
   
   
 public:
@@ -37,7 +37,7 @@ public:
   string getCircuitName() const { return *circuitName; }
   string getDate() const { return *date;}
   
-  void setFinishTime(double newTime) { finishTime = &newTime; }
+  void setFinishTime(string newTime) { finishTime = &newTime; }
   void setName(string nm) { name = &nm; }
   void setIdNum(string newId) { idNum = &newId; }
   void setCircuitName(string newCircuit) { circuitName = &newCircuit; }
@@ -46,24 +46,24 @@ public:
   bool operator > (const Racer &right)
   {
     bool status = false;
-    if(name > right.getName())
+    if(*name > right.getName())
       status = true;
     return status;
   }
   bool operator < (const Racer &right)
   {
     bool status = false;
-    if(name < right.getName())
+    if(*name < right.getName())
       status = true;
     return status;
   }
   const Racer operator=(const Racer &right)
   {
-    name = right.getName();
-    idNum = right.getIdNum();
-    circuitName = right.getCircuitName();
-    date = right.getDate();
-    finishTime = right.getFinishTime();
+    name = &right.getName();
+    idNum = &right.getIdNum();
+    circuitName = &right.getCircuitName();
+    date = &right.getDate();
+    finishTime = &right.getFinishTime();
     return *this;
   }
 };
