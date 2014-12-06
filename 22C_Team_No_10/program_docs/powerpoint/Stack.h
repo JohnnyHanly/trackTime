d774c32a60245289542cc2fde4af611ef09c178d
Steven An <sman@ucdavis.edu>
2014-12-05 22:52:17 -0800

    Fingers crossed, final submission?

---
+++ 22C_Team_No_10/program_docs/powerpoint/Stack.h
@@ -0,0 +1,146 @@
+/**~*~*
+   Stack template
+   Modified by Steven An
+*~*/
+#ifndef DYNAMICSTACK_H
+#define DYNAMICSTACK_H
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Stack
+{
+private:
+   // Structure for the stack nodes
+   struct StackNode
+   {
+      T value;          // Value in the node
+      StackNode *next;  // Pointer to next node
+   };
+
+   StackNode *top;     // Pointer to the stack top
+   int count;
+
+public:
+   //Constructor
+   Stack(){top = NULL; count = 0;}
+
+   // Destructor
+   ~Stack();
+
+   // Stack operations
+   bool push(T);
+   bool pop(T &);
+   bool isEmpty();
+   int getCount();
+   T getTop();
+   void printStack();
+};
+
+//prints stack to screen
+template<class T>
+void Stack<T>::printStack()
+{
+	if(isEmpty())
+  {
+    cout << "-------\nSTACK EMPTY\n-------";
+    return;
+  }
+	StackNode *curPtr;
+	curPtr = top;
+	cout << "\n----------STACK SIZE: " << getCount() << "----------\n";
+	cout << "PRINTING STACK FROM TOP --> BOTTOM\n";
+	
+	while(curPtr != NULL)
+	{
+		cout << curPtr->value << " ";
+		curPtr = curPtr->next;
+	}
+	
+	cout << endl;
+}
+
+//returns top item. 
+template <class T>
+T Stack<T>::getTop()
+{
+	if(!isEmpty())
+		return top->value;
+}
+
+//returns number of items in stack
+template <class T>
+int Stack<T>::getCount()
+{
+  return count;
+}
+
+//d_tor
+template <class T>
+Stack<T>::~Stack()
+{
+   StackNode *currNode, *nextNode;
+
+   // Position nodePtr at the top of the stack.
+   currNode = top;
+
+   // Traverse the list deleting each node.
+   while (currNode) //while (currNode != NULL)
+   {
+      nextNode = currNode->next;
+      delete currNode;
+      currNode = nextNode;
+   }
+}
+
+//pushes item onto stack
+template <class T>
+bool Stack<T>::push(T item)
+{
+   StackNode *newNode; // Pointer to a new node
+
+   // Allocate a new node and store num there.
+   newNode = new StackNode;
+   if (!newNode)
+       return false;
+   newNode->value = item;
+
+   // Update links and counter
+   newNode->next = top;
+   top = newNode;
+
+   count++;
+
+   return true;
+}
+
+//pops, given the stack isnt empty
+template <class T>
+bool Stack<T>::pop(T &item)
+{
+   StackNode *temp; // Temporary pointer
+
+   // empty stack
+   if (isEmpty())
+       return false;
+
+   // pop value off top of stack
+   item = top->value;
+   temp = top->next;
+   delete top;
+   top = temp;
+   count--;
+
+   return true;
+}
+
+/**~*~*
+  Member function isEmpty returns true if the stack
+  is empty, or false otherwise.
+*~**/
+template <class T>
+bool Stack<T>::isEmpty()
+{
+   return count == 0;
+}
+#endif
---
+++ powerpoint/BinaryNode.h
@@ -0,0 +1,36 @@
+// Node for a binary tree
+// Created by Frank M. Carrano and Tim Henry.
+// Modified by: Tim Bly
+// Compiled withe Microsoft Visual Studio 2012 on Windows 8.1 64 bit
+
+#ifndef _BINARY_NODE
+#define _BINARY_NODE
+
+template<class ItemType>
+class BinaryNode
+{   
+private:
+	ItemType              item;         // Data portion
+	BinaryNode<ItemType>* leftPtr;		// Pointer to left child
+	BinaryNode<ItemType>* rightPtr;		// Pointer to right child
+
+public:
+	// constructors
+	BinaryNode() { item = new ItemType(); leftPtr = nullptr; rightPtr = nullptr; }
+	BinaryNode(const ItemType & anItem)	{item = anItem; leftPtr = nullptr; rightPtr = nullptr;}
+	BinaryNode(const ItemType & anItem, 
+		       BinaryNode<ItemType>* left, 
+			   BinaryNode<ItemType>* right) {item = anItem; leftPtr = left; rightPtr = right;}
+	// accessors
+	void setItem(const ItemType & anItem) {item = anItem;}
+	void setLeftPtr(BinaryNode<ItemType>* left) {leftPtr = left;}
+    void setRightPtr(BinaryNode<ItemType>* right) {rightPtr = right;}
+	// mutators
+	ItemType getItem() const	 {return item;}
+	BinaryNode<ItemType>* getLeftPtr() const  {return leftPtr;}
+	BinaryNode<ItemType>* getRightPtr() const {return rightPtr;}
+
+	bool isLeaf() const {return (leftPtr == 0 && rightPtr == 0);}
+}; 
+
+#endif 
